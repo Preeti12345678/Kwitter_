50749b0f9f8c44c74629198b9472e3dac749be67
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-06-02 15:41:30 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -32,7 +32,7 @@ message=message_data['message'];
 like=message_data['like'];
 name_with_tag="<h4 class='name'>"+name+"<img class='user_tick' src='tick.png'></h4>";
 message_with_tag="<h4 class='h4Message'>"+message+"</h4>";
-like_button="<button class='btn btn-warning'id="+message_id+"value="+like+"onclick=updateLike(this.id)'>";
+like_button="<button class='btn btn-warning'id="+firebase_message_id+"value="+like+"onclick=updateLike(this.id)'>";
 span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+like+"</span></button>";
 
 row=name_with_tag+message_with_tag+like_button+span_with_tag;
